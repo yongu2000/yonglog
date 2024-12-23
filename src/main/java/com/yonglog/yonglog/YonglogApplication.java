d64ade6bb3f14b5542858729548a64a8101034fb
Yong U Cho <yongu2000@naver.com>
2024-12-23 13:38:25 +0900

    init commit

---
+++ src/main/java/com/yonglog/yonglog/YonglogApplication.java
@@ -0,0 +1,13 @@
+package com.yonglog.yonglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yonglog/yonglog/YonglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonglog.yonglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
